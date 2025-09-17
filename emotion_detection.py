4805fc61512c5b690454d2a8b161d108bdaac996
FARZAM  NAZARI <134938081+Farzam1372@users.noreply.github.com>
2025-09-17 16:40:15 +0200

    Add files via upload

---
+++ emotion_detection.py
@@ -0,0 +1,18 @@
+import requests
+import json
+
+#Creating a package
+def emotion_detector(text_to_analyse):
+    
+    #End point and header
+    URL = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    Headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+
+    #Input jason
+    input_json = { "raw_document": { "text": text_to_analyse } }
+
+    #Send post request
+    response = requests.post(URL, headers=headers, json=input_json)
+
+    #Return the text
+    return response.text
